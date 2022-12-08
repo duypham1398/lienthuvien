af746b8c41a835d6ec25309ea82a14d9a009900b
Hồ Văn Lân <holan2k@gmail.com>
2022-12-08 10:54:11 +0700

    Update news_screen

--- lib/Screen/news_screen.dart
+++ lib/Screen/news_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:ltv/widget/appbar/appbar_container.dart';
 
 import '../constants/dismension_constants.dart';
+import '../widget/appbar/search.dart';
 import '../widget/appbar/search_with_button.dart';
 import '../widget/appbar/title_with_logo.dart';
+import '../widget/item_news.dart';
 
 class News_Screen extends StatefulWidget {
   const News_Screen({Key? key}) : super(key: key);
@@ -16,16 +18,53 @@ class _News_ScreenState extends State<News_Screen> {
   @override
   Widget build(BuildContext context) {
     return AppBarContainer(
-      title: Column(
-        children: [
-          SizedBox(height: 20),
-          TitleWithLogo(),
-          SizedBox(height: kMediumpading),
-          SearchWithButton(),
-          SizedBox(height: kMediumpading),
-        ],
-      ),
-      child: Column(),
-    );
+        title: Column(
+          children: [
+            SizedBox(height: 20),
+            TitleWithLogo(),
+            SizedBox(height: kMediumpading),
+            Search(),
+            SizedBox(height: kMediumpading),
+          ],
+        ),
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                topLeft: Radius.circular(20),
+                  ),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  ItemNews(
+                    postDate: '12/2/2022',
+                    titleItem: 'Đoàn thư viện Việt Nam tham dự Phiên họp thứ 2',
+                  ),
+                  ItemNews(
+                    postDate: '12/2/2022',
+                    titleItem: 'Đoàn thư viện Việt Nam tham dự Phiên họp thứ 2',
+                  ),
+                  ItemNews(
+                    postDate: '12/2/2022',
+                    titleItem: 'Đoàn thư viện Việt Nam tham dự Phiên họp thứ 2',
+                  ),
+                  ItemNews(
+                    postDate: '12/2/2022',
+                    titleItem: 'Đoàn thư viện Việt Nam tham dự Phiên họp thứ 2',
+                  ),
+                  ItemNews(
+                    postDate: '12/2/2022',
+                    titleItem: 'Đoàn thư viện Việt Nam tham dự Phiên họp thứ 2',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )));
   }
 }
